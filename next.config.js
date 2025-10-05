52d07fe7226b80f7cab663e789a0bd6630fdcefd
Tail <choijiyoung010@gmail.com>
2025-10-05 13:26:03 +0900

    fix : next.config.js css반영 안되는 에러 해결

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
+const repoName = 'Tail418.github.io';
+
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
   trailingSlash: true,
   distDir: 'docs', // ✅ 빌드 결과를 docs에
+  basePath: `/${repoName}`,       // ✅ GitHub Pages용 경로 설정
+  assetPrefix: `/${repoName}/`, 
 };
 export default nextConfig;
